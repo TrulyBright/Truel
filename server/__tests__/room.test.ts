da3703fcfa2f55119a6489d8d72f05be7e3039c3
TrulyBright <trulybright@yonsei.ac.kr>
2023-08-29 19:29:04 +0800

    test: room.full, room.empty, room.private

--- server/__tests__/room.test.ts
+++ server/__tests__/room.test.ts
@@ -3,7 +3,7 @@ import { User } from "@/user"
 import { Room } from "@/room"
 import { CreateRoom, JoinRoom, LeaveRoom } from "@shared/action"
 
-// Scenario #1
+// Scenario #1: Create, Join, Leave
 // 1. A user joins.
 // 2. The user creates a room.
 // 3. Another user joins the room.
@@ -15,25 +15,31 @@ test("Scenario #1", () => {
     const hub = new Hub()
     const user1 = new User("user1")
     const user2 = new User("user2")
-    hub.handleAction(user1, new CreateRoom("room1", 2, null))
+    const password = "password"
+    hub.handleAction(user1, new CreateRoom("room1", 2, password))
     const room = user1.room
-    expect(hub.rooms.has(room!.id)).toBe(true)
+    expect(hub.rooms.has(room.id)).toBe(true)
     expect(room).not.toBeNull()
     expect(room).toBeInstanceOf(Room)
-    expect(room!.title).toBe("room1")
-    expect(room!.host).toBe(user1)
-    expect(room!.members).toContain(user1)
-    expect(room!.members).not.toContain(user2)
+    expect(room.title).toBe("room1")
+    expect(room.host).toBe(user1)
+    expect(room.private).toBe(password !== null)
+    expect(room.members).toContain(user1)
+    expect(room.members).not.toContain(user2)
     hub.handleAction(user2, new JoinRoom(room!.id, room!.password))
-    expect(room!.host).toBe(user1)
-    expect(room!.members).toContain(user1)
-    expect(room!.members).toContain(user2)
+    expect(room.host).toBe(user1)
+    expect(room.members).toContain(user1)
+    expect(room.members).toContain(user2)
+    expect(room.full).toBe(true)
     hub.handleAction(user1, new LeaveRoom())
-    expect(room!.host).toBe(user2)
-    expect(room!.members).not.toContain(user1)
-    expect(room!.members).toContain(user2)
+    expect(room.host).toBe(user2)
+    expect(room.members).not.toContain(user1)
+    expect(room.members).toContain(user2)
     hub.handleAction(user2, new LeaveRoom())
-    expect(room!.members).not.toContain(user1)
-    expect(room!.members).not.toContain(user2)
-    expect(hub.rooms.has(room!.id)).toBe(false)
+    expect(room.members).not.toContain(user1)
+    expect(room.members).not.toContain(user2)
+    expect(hub.rooms.has(room.id)).toBe(false)
+    expect(room.empty).toBe(true)
 })
+
+// Scenario #2
