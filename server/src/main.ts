f45c109aec0257eaf4921cb474673a6f468fa411
TrulyBright <trulybright@yonsei.ac.kr>
2023-09-18 19:43:36 +0800

    specify type

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,13 +2,14 @@ import { WebSocketServer } from "ws"
 import { Hub } from "@/hub"
 import { User } from "@/user"
 import { actionConstructors } from "@shared/action"
+import { GameEvent } from "@shared/event"
 
 const wss = new WebSocketServer({ port: 8080 })
 const hub = new Hub()
 
 wss.on("connection", (ws) => {
     const user = new User("test")
-    user.on("GameEvent", event => {
+    user.on("GameEvent", (event: GameEvent) => {
         const data = JSON.stringify({
             type: event.constructor.name,
             args: event
