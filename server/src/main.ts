1107b6e9a8a5ff1b942862948705aa94c47a395f
TrulyBright <trulybright@yonsei.ac.kr>
2023-10-10 16:46:09 +0800

    strictly typing main

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,7 +4,7 @@ import path from "path"
 
 config({ path: path.resolve(__dirname, "../.env") })
 
-const endpoint = new WebSocketEndpoint(process.env.HOSTNAME, Number(process.env.PORT))
+const endpoint = new WebSocketEndpoint(process.env.HOSTNAME!, Number(process.env.PORT))
 endpoint.start()
 endpoint.addDummyUsers(256)
 endpoint.addDummyRooms()
