3382ea693be6cbeac21322c30b1eee21c6d448ed
TrulyBright <trulybright@yonsei.ac.kr>
2023-10-13 22:57:33 +0800

    dummy from 256 to 10

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,5 +6,5 @@ config({ path: path.resolve(__dirname, "../.env") })
 
 const endpoint = new WebSocketEndpoint(process.env.HOSTNAME!, Number(process.env.PORT))
 endpoint.start()
-endpoint.hub.addDummyUsers(256)
+endpoint.hub.addDummyUsers(10)
 endpoint.hub.addDummyRooms()
