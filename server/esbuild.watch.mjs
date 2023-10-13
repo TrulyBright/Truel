54f459c8b523cf546087bb04c65694c591a99d6b
TrulyBright <trulybright@yonsei.ac.kr>
2023-10-13 21:01:17 +0800

    npm run watch in /server now works

--- server/esbuild.watch.mjs
+++ server/esbuild.watch.mjs
@@ -1,4 +1,5 @@
 import esbuild from "esbuild"
+import { spawn } from "child_process"
 import options from "./esbuild.options.mjs"
 
 const context = await esbuild.context({
@@ -7,12 +8,15 @@ const context = await esbuild.context({
         name: "restart-server",
         setup({onStart, onEnd}) {
             let t
+            let serverProcess = undefined
             onStart(() => {
-                console.log("Rebuilding the server...")
+                console.log(`${serverProcess ? "Rebuilding" : "Building"} the server...`)
                 t = Date.now()
+                serverProcess?.kill()
             })
             onEnd(() => {
-                console.log(`Server rebuilt in ${Date.now() - t} ms. Restarting the server...`)
+                console.log(`Server ${serverProcess ? "rebuilt" : "built"} in ${Date.now() - t} ms. ${serverProcess ? "Restarting" : "Starting"} the server...`)
+                serverProcess = spawn("node", [options.outfile])
             })
         }
     }]
